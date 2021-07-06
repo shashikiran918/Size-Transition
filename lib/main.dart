2dff5ebf0301625b5f8d00ad8e6a0309284d7c44
shashikiran918 <85386298+shashikiran918@users.noreply.github.com>
2021-07-06 19:20:51 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+/// Flutter code sample for SizeTransition
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp( MyApp());
+
+/// This is the main application widget.
+class MyApp extends StatelessWidget {
+
+  static const String _title = 'SIZE TRANSITION';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+/// AnimationControllers can be created with `vsync: this` because of TickerProviderStateMixin.
+class _MyStatefulWidgetState extends State<MyStatefulWidget>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 3),
+    vsync: this,
+  )..repeat();
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.fastOutSlowIn,
+  );
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizeTransition(
+        sizeFactor: _animation,
+        axis: Axis.horizontal,
+        axisAlignment: -1,
+        child: const Center(
+          child: FlutterLogo(size: 200.0),
+        ),
+      ),
+    );
+  }
+}
